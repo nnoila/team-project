531692b7a7503fa16d54075ef524b69cbd3ca0ae
nnoila <naila.anindya.rusdi@gmail.com>
2025-11-21 17:18:53 -0500

    Implemented example Transaction class

---
+++ src/main/java/ai_insights/Transaction.java
@@ -0,0 +1,51 @@
+package ai_insights;
+
+import java.time.LocalDate;
+
+public class Transaction {
+     public String userId;
+     public LocalDate date;
+     public String description;
+     public int transactionId;
+     public String category;
+
+     public String getUserId() {
+         return this.userId;
+     }
+
+     public LocalDate getDate() {
+         return this.date;
+     }
+
+     public String getDescription() {
+         return this.description;
+     }
+
+     public int getTransactionId() {
+         return this.transactionId;
+     }
+
+     public String getCategory() {
+         return this.category;
+     }
+
+     public void setUserId(String userId) {
+         this.userId = userId;
+     }
+
+     public void setDate(LocalDate date) {
+         this.date = date;
+     }
+
+     public void setDescription(String description) {
+         this.description = description;
+     }
+
+     public void setTransactionId(int transactionId) {
+         this.transactionId = transactionId;
+     }
+
+     public void setCategory(String category) {
+         this.category = category;
+     }
+}
