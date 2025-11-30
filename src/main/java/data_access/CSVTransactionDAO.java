85846b6ff193cdc138dec8fd491d08068d0cc5d1
ghinanahlawi <ghina_nahlawi@icloud.com>
2025-11-30 11:33:08 -0500

    Fixing if the csv file already has data

--- src/main/java/data_access/CSVTransactionDAO.java
+++ src/main/java/data_access/CSVTransactionDAO.java
@@ -76,6 +76,11 @@ public class CSVTransactionDAO implements TransactionDataAccess {
              BufferedWriter bw = new BufferedWriter(fw);
              PrintWriter out = new PrintWriter(bw)) {
 
+            File file = new File(csvFilePath);
+            if (file.length() > 0) {
+                bw.newLine();
+            }
+
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
             out.printf("%s,%s,%.2f,%s,%s%n",
